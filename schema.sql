8445b380a0adba05bd5c1e4a1d2a27a297036fd4
Kelly Pressley <kellypressley86@gmail.com>
2019-11-06 10:23:03 -0700

    added all JS files and created the department table

--- schema.sql
+++ schema.sql
@@ -3,7 +3,6 @@ CREATE DATABASE bamazon_db;
 USE bamazon_db;
 
 CREATE TABLE products (
-  
   item_id INTEGER AUTO_INCREMENT NOT NULL,
   product_name VARCHAR(100),
   department_name VARCHAR(100),
@@ -11,4 +10,14 @@ CREATE TABLE products (
   price DECIMAL (6,2),
   
   PRIMARY KEY(item_id)
+);
+
+CREATE TABLE departments (
+    department_id INTEGER AUTO_INCREMENT NOT NULL,
+    department_name VARCHAR(100),
+    over_head_costs DECIMAL (8,2),
+    product_sales DECIMAL (8,2),
+    total_profit DECIMAL (8,2),
+
+    PRIMARY KEY(department_id)
 );
